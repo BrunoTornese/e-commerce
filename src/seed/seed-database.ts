4cd1b32a24b2af5b1c4ecfa7d27698ef9fd9952e
Bruno Tornese <brunoluchi2003@gmail.com>
2024-09-11 23:00:34 +0200

    arreglo de nombres erroneos en el schema de prisma

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,16 +1,53 @@
 import prisma from "../lib/prisma";
+import { initialData } from "./seed";
 
 async function main() {
-  await Promise.all([
-    prisma.productImage.deleteMany(),
-    prisma.product.deleteMany(),
-    prisma.category.deleteMany(),
-  ]);
+  // Delete all data
+  await prisma.productImage.deleteMany(),
+    await prisma.product.deleteMany(),
+    await prisma.category.deleteMany();
+
+  const { categories, products } = initialData;
+
+  // Insert categories
+  const categoriesData = categories.map((category) => ({ name: category }));
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap: Record<string, string> = {};
+
+  categoriesDB.forEach((category) => {
+    categoriesMap[category.name.toLowerCase()] = category.id;
+  });
+
+  // Insert products
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    //Insert images
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
+  });
 
   console.log("🚀 Seed executed");
 }
 
-() => {
+(async () => {
   if (process.env.NODE_ENV === "production") return;
-  main();
-};
+  await main();
+})();
