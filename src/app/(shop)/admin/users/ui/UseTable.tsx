de62723f032b9b594a341dd7a61f4bc13d1bb715
Bruno Tornese <brunoluchi2003@gmail.com>
2024-12-07 03:07:40 +0100

    eliminado de columna Update Role

--- src/app/(shop)/admin/users/ui/UseTable.tsx
+++ src/app/(shop)/admin/users/ui/UseTable.tsx
@@ -30,12 +30,6 @@ export const UseTable = ({ users }: Props) => {
             >
               Role
             </th>
-            <th
-              scope="col"
-              className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-            >
-              Update Role
-            </th>
           </tr>
         </thead>
         <tbody>
