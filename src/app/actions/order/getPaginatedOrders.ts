7e3634a428bba80a4e0e592078ead35a449dd592
Bruno Tornese <brunoluchi2003@gmail.com>
2024-12-25 16:42:24 +0100

    agregado de logica para menejar error

--- src/app/actions/order/getPaginatedOrders.ts
+++ src/app/actions/order/getPaginatedOrders.ts
@@ -5,8 +5,10 @@ import prisma from "@/lib/prisma";
 
 export const getPaginetedOrders = async () => {
   const session = await auth();
+  console.log("User session:", session);
 
   if (session?.user.role !== "admin") {
+    console.error("User is not admin");
     return {
       ok: false,
       message: "You must be an admin to view this page",
